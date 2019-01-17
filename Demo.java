def1467428452b3245a369128ac1f79b86dc5bda
FelixvL <digitaleservice@gmail.com>
2019-01-17 15:40:47 +0100

    punt 2

--- Demo.java
+++ Demo.java
@@ -1,10 +1,27 @@
 public class Demo{
 	public static void main( String[] args ){
 		Team team1 = new Team();
-		System.out.println(team1);
+		team1.naam = "white walker";
+		Team team2 = new Team();
+		team2.naam = "Ice Ice Babys";
+		Wedstrijd wedstrijd = new Wedstrijd(team1, team2);
+		wedstrijd.aankondigen();
 		
 	}
 }
+class Wedstrijd{
+	Team team1;
+	Team team2;
+	Team winnaar;
+	Wedstrijd(Team thuisteam , Team uitteam){
+		team1 = thuisteam;
+		team2 = uitteam;
+	}
+	void aankondigen() {
+		System.out.println("Er vind een wedstrijd plaats tussen "+ team1.naam + " en " + team2.naam);
+	}
+}
+
 
 class Team{
 	String naam;
