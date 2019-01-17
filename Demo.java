bfb726b743a78aab00c5452ab05d8ece618b5e3b
FelixvL <digitaleservice@gmail.com>
2019-01-17 16:34:57 +0100

    vijfde

--- Demo.java
+++ Demo.java
@@ -23,42 +23,77 @@ public class Demo{
 		wedstrijd3.aankondigen();
 		wedstrijd3.spelen();
 		wedstrijd3.winnaarBekendMaken();
+		Team teamGoud = wedstrijd3.winnaar;
+		Team teamZilver = wedstrijd3.verliezerGeven();
+		Wedstrijd wedstrijd4 = new Wedstrijd(wedstrijd.verliezerGeven(), wedstrijd2.verliezerGeven());
+		wedstrijd4.aankondigen();
+		wedstrijd4.spelen();
+		wedstrijd4.winnaarBekendMaken();
+		Team teamBrons = wedstrijd4.winnaar;
+		Ceremonie c = new Ceremonie(teamGoud, teamZilver, teamBrons);
+		c.uitkeren();
+		//Team teamZilver = 
+		
+		
 	}
 }
 class Wedstrijd{
-	Team team1;
-	Team team2;
+	Team teama;
+	Team teamb;
 	Team winnaar;
 	Wedstrijd(Team thuisteam , Team uitteam){
-		team1 = thuisteam;
-		team2 = uitteam;
+		teama = thuisteam;
+		teamb = uitteam;
 	}
 	public void winnaarBekendMaken() {
-		if(team1.score > team2.score) {
-			System.out.println(team2.naam+" heeft gewonnen met de score"+team2.score);
-			winnaar = team2;
-		}else if(team1.score < team2.score){
-			System.out.println(team1.naam+" heeft gewonnen met de score"+team1.score);			
-			winnaar = team1;
-		}else if(team1.score == team2.score) {
+		if(teama.score > teamb.score) {
+			System.out.println(teamb.naam+" heeft gewonnen met de score"+teamb.score);
+			winnaar = teamb;
+		}else if(teama.score < teamb.score){
+			System.out.println(teama.naam+" heeft gewonnen met de score"+teama.score);			
+			winnaar = teama;
+		}else if(teama.score == teamb.score) {
 			System.out.println("Iedereen heeft gewonnen.");
 			spelen();
+			winnaarBekendMaken();
 		}
 		
 	}
+	public Team verliezerGeven() {
+		if(winnaar == teama) {
+			return teamb;
+		}
+		return teama;
+	}
 	public void spelen() {
 		Random random = new Random();
-		int wedstrijdtijd = random.nextInt(1000);
-		team1.score = wedstrijdtijd;
+		int wedstrijdtijd = random.nextInt(3);
+		teama.score = wedstrijdtijd;
 		Random random2 = new Random();
-		int wedstrijdtijd2 = random2.nextInt(1000);
-		team2.score = wedstrijdtijd2;		
+		int wedstrijdtijd2 = random2.nextInt(3);
+		teamb.score = wedstrijdtijd2;		
 	}
 	void aankondigen() {
-		System.out.println("Er vind een wedstrijd plaats tussen "+ team1.naam + " en " + team2.naam);
+		System.out.println("Er vind een wedstrijd plaats tussen "+ teama.naam + " en " + teamb.naam);
 	}
 }
 
+class Ceremonie{
+	private Team teamGoud;
+	private Team teamZilver;
+	private Team teamBrons;
+	Ceremonie(Team teama, Team teamb, Team teamc){
+		teamGoud = teama;
+		teamZilver = teamb;
+		teamBrons = teamc;
+	}
+	void uitkeren() {
+		System.out.println("Goud voor:"+teamGoud.naam);
+		System.out.println("Zilver voor:"+teamZilver.naam);
+		System.out.println("Brons voor:"+teamBrons.naam);
+	}
+	
+}
 
 class Team{
 	String naam;
