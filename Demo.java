a07b783531c4ee4eb2f06fe7b29789e0f842dbf1
FelixvL <digitaleservice@gmail.com>
2019-01-17 15:29:31 +0100

    start

---
+++ Demo.java
@@ -0,0 +1,15 @@
+public class Demo{
+	public static void main( String[] args ){
+		Team team1 = new Team();
+		System.out.println(team1);
+		
+	}
+}
+
+class Team{
+	String naam;
+	int score;
+	boolean verloren = false;
+}
+
+
