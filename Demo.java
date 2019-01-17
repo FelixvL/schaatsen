2ddb89ee565b4ac48a24bf461259bfbc29c56e47
FelixvL <digitaleservice@gmail.com>
2019-01-17 15:54:42 +0100

    derde

--- Demo.java
+++ Demo.java
@@ -1,3 +1,5 @@
+import java.util.Random;
+
 public class Demo{
 	public static void main( String[] args ){
 		Team team1 = new Team();
@@ -6,7 +8,8 @@ public class Demo{
 		team2.naam = "Ice Ice Babys";
 		Wedstrijd wedstrijd = new Wedstrijd(team1, team2);
 		wedstrijd.aankondigen();
-		
+		wedstrijd.spelen();
+		wedstrijd.winnaarBekendMaken();
 	}
 }
 class Wedstrijd{
@@ -16,6 +19,24 @@ class Wedstrijd{
 	Wedstrijd(Team thuisteam , Team uitteam){
 		team1 = thuisteam;
 		team2 = uitteam;
+	}
+	public void winnaarBekendMaken() {
+		if(team1.score > team2.score) {
+			System.out.println(team2.naam+" heeft gewonnen met de score"+team2.score);
+		}else if(team1.score < team2.score){
+			System.out.println(team1.naam+" heeft gewonnen met de score"+team1.score);			
+		}else if(team1.score == team2.score) {
+			System.out.println("Iedereen heeft gewonnen.");
+		}
+		
+	}
+	public void spelen() {
+		Random random = new Random();
+		int wedstrijdtijd = random.nextInt(1000);
+		team1.score = wedstrijdtijd;
+		Random random2 = new Random();
+		int wedstrijdtijd2 = random2.nextInt(1000);
+		team2.score = wedstrijdtijd2;		
 	}
 	void aankondigen() {
 		System.out.println("Er vind een wedstrijd plaats tussen "+ team1.naam + " en " + team2.naam);
